53bf00a957b58cf5e77119469bec46903173f3ee
Ami Tanaka <amiless1234l9@gmail.com>
2024-12-12 00:53:35 +0900

    devise実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+ #ユーザー登録やログイン認証等を使う前に、configure_permitted_parametersメソッドが実行される
+  protected
+  # 「private」ではなく「protected」にすることで、他のコントローラーからも参照できるようにしている
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
