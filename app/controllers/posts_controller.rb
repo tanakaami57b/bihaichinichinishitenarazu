a7146511036521813cb55678d91eae895f110a9c
Ami Tanaka <amiless1234l9@gmail.com>
2024-12-12 13:39:26 +0900

    feature/投稿機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,12 +2,33 @@ class PostsController < ApplicationController
     # ログインを必須にしないアクションを指定
   skip_before_action :authenticate_user!, only: [:index, :show]
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to posts_path, notice: "投稿しました！"
+    else
+      flash.now[:alert] = "投稿に失敗しました"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
     def index
         @posts = Post.includes(:user)
       end
 
     def show
-      @post = Post.find(:id)
+      @post = Post.find(params[:id]) # 修正: params[:id] を使用
       @user = @post.user
     end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content, :required_time, :required_cost, :parts, :image_url)
   end
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,30 @@
 class Post < ApplicationRecord
+    enum required_time: {
+    three_min: 0,   # 3分
+    five_min: 1,    # 5分
+    ten_min: 2,     # 10分
+    thirty_min: 3,  # 30分
+    one_hour: 4,    # 1時間
+    one_and_half_hour: 5, # 1時間半
+    over_two_hours: 6     # 2時間以上
+  }
+    enum required_cost: {
+        free: 0,            # 0円
+        under_1000: 1,      # 1000円未満
+        from_1000_to_5000: 2, # 1000円以上5000円以内
+        from_5000_to_10000: 3, # 5000円以上10000円以内
+        over_10000: 4       # 10000円以上
+      }
+      enum parts: {
+        skin: 0,
+        head: 1,
+        face: 2,
+        upper_body: 3,
+        lower_body: 4,
+        whole_body: 5,
+        other: 6
+      }
+
     validates :title, presence: true, length: { maximum: 255 }
     validates :content, presence: true, length: { maximum: 65_535 }
 
