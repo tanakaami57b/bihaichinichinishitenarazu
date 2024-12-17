cef38cb89a9182404a91bab1af0d334542ada3d8
Ami Tanaka <amiless1234l9@gmail.com>
2024-12-18 00:00:27 +0900

    fix:ブックマーク解除後、ブックマーク一覧に遷移するように修正

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -4,16 +4,16 @@ class BookmarksController < ApplicationController
     def create
       post = Post.find(params[:post_id])
       current_user.bookmark(post)
-      redirect_to posts_path, notice: "これやる！リストに追加しました！"
+      redirect_to bookmarks_path, notice: "これやる！リストに追加しました！"
     end
 
     def destroy
-        bookmark = current_user.bookmarks.find_by(id: params[:id])
-        if bookmark
-          bookmark.destroy
-          redirect_to posts_path, notice: "これやる！リストを解除しました！", status: :see_other
-        else
-          redirect_to posts_path, alert: "ブックマークが見つかりませんでした。", status: :unprocessable_entity
-        end
+      bookmark = current_user.bookmarks.find_by(id: params[:id])
+      if bookmark
+        bookmark.destroy
+        redirect_to bookmarks_path, notice: "これやる！リストを解除しました！", status: :see_other
+      else
+        redirect_to bookmarks_path, alert: "ブックマークが見つかりませんでした。", status: :unprocessable_entity
       end
+    end
   end
